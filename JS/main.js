c298a56f50c1191bcc849adf88d6af734f6606ff
Juan <juand-rojas@hotmail.com>
2019-02-17 23:42:31 -0500

    Updated form styling

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,8 @@
-var test;
+function displayTaskCreator() {
+  var x = document.getElementById("tCreator");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
